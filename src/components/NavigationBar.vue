5fc8d7c6fb6e5d2ceb4367b340754384bc6bbb6c
John Bonjour <whitewolfzhang@gmail.com>
2024-02-17 22:22:19 -0800

    moved navbar outside HomeView as independent component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar"></nav>
+</template>
+
+<style scoped>
+.navbar {
+  height: 5em;
+  background: darkseagreen;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,14 @@
 <script setup>
 import ContentDirectory from "../components/ContentDirectory.vue"
+import NavigationBar from "@/components/NavigationBar.vue";
 </script>
 
 <template>
-  <nav class="navbar"></nav>
+  <NavigationBar/>
   <main>
     <ContentDirectory/>
   </main>
 </template>
 
 <style scoped>
-.navbar {
-  height: 5em;
-  background: darkseagreen;
-}
 </style>
