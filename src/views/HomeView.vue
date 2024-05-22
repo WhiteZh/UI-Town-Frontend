0bc3375743d0c01e0348405b4969d2f304bcbab9
Liva <gyx2009olivia@gmail.com>
2024-05-21 20:38:58 -0700

    Embedded ContentDirectory inside HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,38 @@
 <script setup>
-import ContentDirectory from "../components/ContentDirectory.vue"
 import NavigationBar from "@/components/NavigationBar.vue";
+import DropDown from "@/components/DropDown.vue";
 </script>
 
 <template>
-  <NavigationBar/>
-  <main>
-    <ContentDirectory/>
-  </main>
+    <NavigationBar/>
+    <main>
+        <div style="height: 5rem;"></div>
+        <DropDown><template v-slot:title>ALL</template></DropDown>
+        <DropDown>
+          <template v-slot:title>CSS</template>
+          <template v-slot:menu>
+            <p>hi</p>
+          </template>
+        </DropDown>
+        <DropDown>
+          <template v-slot:title>JavaScript</template>
+          <template v-slot:menu>
+            <p>hi</p>
+          </template>
+        </DropDown>
+    </main>
 </template>
 
 <style scoped>
+main {
+  height: 100vh;
+  max-width: 20em;
+  width: 30vw;
+  display: flex;
+  flex-direction: column;
+  padding-left: 2rem;
+}
+
+
+
 </style>
