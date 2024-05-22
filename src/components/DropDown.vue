513e0596da1f145d676c7039ed6dc7ee4e35f5f4
Liva <gyx2009olivia@gmail.com>
2024-05-21 20:43:23 -0700

    Added DropDown component

---
+++ src/components/DropDown.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="drop-down-container">
+    <div class="drop-down-btn">
+      <slot name="title"/>
+    </div>
+    <div class="drop-down-menu"><slot name="menu"/></div>
+  </div>
+</template>
+
+<style scoped>
+.drop-down-container {
+  position: relative;
+}
+
+.drop-down-btn {
+  padding: 0.5rem 1rem;
+  margin: 0;
+  width: 8rem;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: white;
+  text-align: left;
+}
+
+.drop-down-btn:hover {
+  background-color: #272030;
+  border-radius: 2rem
+}
+
+.drop-down-container:hover .drop-down-menu {
+  display: inline-block;
+}
+
+.drop-down-menu {
+  position: absolute;
+  max-width: 10rem;
+  color: white;
+  display: none;
+}
+</style>
