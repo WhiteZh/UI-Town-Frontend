a1da6f29cf140089a1d647fbc60f9b54bbe8fa9c
Liva <gyx2009olivia@gmail.com>
2024-05-25 03:13:58 -0700

    add 1 title and subtitle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,29 +20,57 @@
 </script>
 
 <template>
-    <NavigationBar/>
-    <aside>
+  <NavigationBar/>
+  <div id="container">
+    <div id="left-nav">
         <div style="height: 10rem;"></div>
         <DropDown>All</DropDown>
         <DropDown :list="CSS_attribute">CSS</DropDown>
         <DropDown :list="JS_attribute">JavaScript</DropDown>
-    </aside>
+    </div>
+    <div id="content">
+      <h3>To Select</h3>
+      <h6>Choose the code of your choice</h6>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
-aside {
-  height: 100%;
-  width: 24rem;
+#container {
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 3.6rem);
+}
+
+#content {
+  flex-grow: 1;
+}
+
+#content>h3 {
+  font-size: 1.75rem;
+  color: #D0C3F1;
+  text-indent: 2rem;
+}
+
+#content>h6 {
+  color: #D0C3F1;
+  letter-spacing: 0.2rem;
+  font-family: Cooljazz, sans-serif;
+  font-weight: lighter;
+  font-style: italic;
+  text-indent: 2rem;
+}
+
+#left-nav {
+  width: 20rem;
   display: flex;
   flex-direction: column;
-  margin-left: 2rem;
+  margin-left: 1rem;
   align-items: start;
 }
 
-@media (max-width: 100rem) {
-  aside {
-    display: none;
-  }
-}
+
+
 
 </style>
